cb9c6d5bec0cb3f7d2979a9c2453a961b2e77fac
Abubakr Shakirov <musayid@outlook.com>
2026-06-17 10:13:43 +0300

    added tasks from 15 th homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { training } from './training';
+import { Color } from '../enums/Color';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,38 @@ import { training } from './training';
 })
 export class AppComponent {
 
-  constructor() {
-    console.log(training);
+  companyName: string = 'РУМТИБЕТ'
+
+
+  isPrimaryColor(color: Color): boolean {
+    return (
+      color === Color.RED ||
+      color === Color.GREEN ||
+      color === Color.BLUE
+    );
   }
 
+  saveLastVisit(): void {
+    localStorage.setItem(
+      'lastVisit',
+      new Date().toString()
+    );
+  }
 
+  saveAmountVisit(): void {
+    const visits = localStorage.getItem('amountVisit');
+
+    const count = visits ? Number(visits) : 0;
+
+    localStorage.setItem(
+      'amountVisit',
+      String(count + 1)
+    );
+  }
+
+  constructor() {
+    this.saveLastVisit();
+
+    this.saveAmountVisit();
+  }
 }
--- src/app/training.ts
+++ src/app/training.ts
@@ -1,24 +1,17 @@
-// 2. Создать файл в папке "app" под названием "training.ts", подключить его в app.component.ts.
 export const training = 'Angular training';
 
 
-// 3. Создать функцию, которая принимает два числа и возвращает их сумму.
 function sum(a: number, b: number): number {
     return a + b;
 }
 
-console.log(sum(5, 10));
 
-
-// 4. Создать переменную uploadStatus
 let uploadStatus: 'loading' | 'success' | 'error';
 
 
-// 5. Создать переменную textFormat
 let textFormat: 'uppercase' | 'lowercase' | 'capitalize';
 
 
-// 6. Создать интерфейс юзера
 interface User {
     id: number;
     name: string;
@@ -26,14 +19,12 @@ interface User {
     age?: number;
 }
 
-// 7. Создать интерфейс, который расширяется интерфейсом User
 interface AdminUser extends User {
     role: string;
     permissions: string[]; // чтобы админ мог менять, удалять и тд
 }
 
 
-// 8. Создать функцию, которая принимает строку и вариант
 function formatText(
     text: string,
     format: 'uppercase' | 'lowercase' | 'capitalize'
@@ -41,31 +32,23 @@ function formatText(
 
     if (format === 'uppercase') {
         return text.toUpperCase();
-    }
-
-    if (format === 'lowercase') {
+    } else if (format === 'lowercase') {
         return text.toLowerCase();
-    }
-
-    if (format === 'capitalize') {
+    } else if (format === 'capitalize') {
         return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
     }
-    
+
     return text;
 }
 
-console.log(formatText('hello world', 'capitalize'));
 
 
-// 9. Создать функцию, которая принимает строку и символ
 function removeSym(text: string, sym: string): string {
     return text.replaceAll(sym, '');
 }
 
-console.log(removeSym('hello world', 'l'))
 
 
-// 10. Создать массив объектов на основе интерфейса с задания №6
 const users: User[] = [
     {
         id: 1,
@@ -87,6 +70,4 @@ const users: User[] = [
     }
 ];
 
-const filteredUsers = users.filter(user => user.age === 20);
-
-console.log(filteredUsers);
+const filteredUsers = users.filter((user: User) => user.age === 20);
